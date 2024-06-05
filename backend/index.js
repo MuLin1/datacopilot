66a49dd8f8f1c266f0aadaa0456556449c9943e7
chd <1039421948@qq.com>
2024-06-05 16:09:08 +0800

    响应前端sql

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
+const PORT = 3000;
 
-app.use(express.json()); // 用于解析JSON格式的请求体
+// 解析请求体中的 JSON 数据
+app.use(bodyParser.json());
 
 // 处理跨域请求问题
 const cors = require('cors');
@@ -13,7 +17,43 @@ app.post('/register', (req, res) => {
   res.status(200).send('注册成功');
 });
 
-const PORT = 3000;
+app.post('/sql',(req,res)=>{
+console.log('请求的sql',req.body);
+const { sql } = req.body;
+//数据库连接查询
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port:3308,
+  user: 'root',
+  password: 'chd123456',
+  database: 'todomvc',});
+
+
+
+// 连接到MySQL数据库
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database');
+});
+
+// 执行SQL查询
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).json({ error: 'Error executing query' });
+      return;
+    }
+
+    // 返回查询结果
+    res.json(results);
+  });
+
+});
+
+
 app.listen(PORT, () => {
   console.log(`服务器运行在 http://localhost:${PORT}`);
 });
